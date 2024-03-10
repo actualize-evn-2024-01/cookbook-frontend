115a587f56d2e67b13333e2ea50cd996c75b4c82
Peter Jang <peter.x.jang@gmail.com>
2024-03-10 11:46:13 -0500

    Style recipes as cards

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -6,14 +6,16 @@ export function RecipesIndex(props) {
     <div id="recipes-index">
       <h1>All recipes</h1>
       <p>The name is {props.name}</p>
-      {props.recipes.map((recipe) => (
-        <div key={recipe.id} className="recipes">
-          <h2>{recipe.title}</h2>
-          <img src={recipe.image_url} alt="" />
-          <p>Chef: {recipe.chef}</p>
-          <button>More info</button>
-        </div>
-      ))}
+      <div className="cards">
+        {props.recipes.map((recipe) => (
+          <div key={recipe.id} className="recipes card">
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image_url} alt="" />
+            <p>Chef: {recipe.chef}</p>
+            <button>More info</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
