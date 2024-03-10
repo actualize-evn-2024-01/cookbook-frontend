29176ffb57d228fda48daf41ec1f388c00388b31
Peter Jang <peter.x.jang@gmail.com>
2024-03-10 11:28:25 -0500

    Style header and footer

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,9 @@
 export function Header() {
   return (
     <header>
-      <a href="#">Home</a> | <a href="#recipes-index">All recipes</a> | <a href="#recipes-new">New recipe</a>
+      <a href="#">Home</a>
+      <a href="#recipes-index">All recipes</a>
+      <a href="#recipes-new">New recipe</a>
     </header>
   );
 }
