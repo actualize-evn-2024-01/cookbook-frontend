c099501ab2de0cbc22feec94390e3b4a4fd906b8
Peter Jang <peter.x.jang@gmail.com>
2024-03-12 18:24:17 -0500

    Create a recipe using a pure HTML form

--- src/RecipesNew.jsx
+++ src/RecipesNew.jsx
@@ -2,12 +2,24 @@ export function RecipesNew() {
   return (
     <div id="recipes-new">
       <h1>New recipe</h1>
-      <form>
+      <form method="POST" action="http://localhost:3000/recipes.json">
         <div>
-          Title: <input type="text" />
+          Title: <input name="title" type="text" />
         </div>
         <div>
-          Chef: <input type="text" />
+          Chef: <input name="chef" type="text" />
+        </div>
+        <div>
+          Image url: <input name="image_url" type="text" />
+        </div>
+        <div>
+          Prep time: <input name="prep_time" type="text" />
+        </div>
+        <div>
+          Ingredients: <input name="ingredients" type="text" />
+        </div>
+        <div>
+          Directions: <input name="directions" type="text" />
         </div>
         <button type="submit">Create recipe</button>
       </form>
