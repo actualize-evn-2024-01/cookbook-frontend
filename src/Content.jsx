7ae99cdd0b83b9d73e94de02331462c1cf4d7c8a
Peter Jang <peter.x.jang@gmail.com>
2024-03-11 19:36:00 -0500

    Use web request to backend instead of fake data

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,30 +1,24 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { RecipesIndex } from "./RecipesIndex";
 import { RecipesNew } from "./RecipesNew";
 
 export function Content() {
   let name = "PETER!!!";
 
-  let recipes = [
-    {
-      id: 1,
-      title: "Raw Eggs!!!",
-      chef: "Peter Jang",
-      image_url: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg",
-    },
-    {
-      id: 2,
-      title: "Mud Pie",
-      chef: "Jay Wengrow",
-      image_url: "https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg",
-    },
-    {
-      id: 3,
-      title: "Pizza",
-      chef: "Jay Wengrow",
-      image_url:
-        "https://static.onecms.io/wp-content/uploads/sites/9/2021/06/15/mozzarella-pizza-margherita-FT-RECIPE0621.jpg",
-    },
-  ];
+  // let recipes = [];
+  const [recipes, setRecipes] = useState([]);
+
+  const handleIndexRecipes = () => {
+    axios.get("http://localhost:3000/recipes.json").then((response) => {
+      console.log("THE DATA FROM THE BACKEND: ", response);
+      // recipes = response.data;
+      setRecipes(response.data);
+    });
+  };
+
+  // handleIndexRecipes();
+  useEffect(handleIndexRecipes, []);
 
   return (
     <main>
