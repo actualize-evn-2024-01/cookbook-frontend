fb471cc30986ceb2819fc9c7bd3430024d7313ff
Peter Jang <peter.x.jang@gmail.com>
2024-03-14 18:39:42 -0500

    Make cards and images same height

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -10,8 +10,8 @@ export function RecipesIndex(props) {
       <div className="row row-cols-1 row-cols-md-4 g-4">
         {props.recipes.map((recipe) => (
           <div key={recipe.id} className="col">
-            <div className="card">
-              <img src={recipe.image_url} className="card-img-top" alt="..." />
+            <div className="card h-100">
+              <img src={recipe.image_url} className="card-img-top object-fit-cover" height="200" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{recipe.title}</h5>
                 <p className="card-text">Chef: {recipe.chef}</p>
