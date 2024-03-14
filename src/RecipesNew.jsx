5f8e78f5cd69d957c6fcd8f6fe8f1cc8134edd72
Peter Jang <peter.x.jang@gmail.com>
2024-03-14 18:27:46 -0500

    Use bootstrap to style new recipe form

--- src/RecipesNew.jsx
+++ src/RecipesNew.jsx
@@ -12,25 +12,27 @@ export function RecipesNew(props) {
     <div id="recipes-new">
       <h1>New recipe</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          Title: <input name="title" type="text" />
+        <div className="mb-3">
+          Title: <input className="form-control" name="title" type="text" />
         </div>
-        <div>
-          Chef: <input name="chef" type="text" />
+        <div className="mb-3">
+          Chef: <input className="form-control" name="chef" type="text" />
         </div>
-        <div>
-          Image url: <input name="image_url" type="text" />
+        <div className="mb-3">
+          Image url: <input className="form-control" name="image_url" type="text" />
         </div>
-        <div>
-          Prep time: <input name="prep_time" type="text" />
+        <div className="mb-3">
+          Prep time: <input className="form-control" name="prep_time" type="text" />
         </div>
-        <div>
-          Ingredients: <input name="ingredients" type="text" />
+        <div className="mb-3">
+          Ingredients: <input className="form-control" name="ingredients" type="text" />
         </div>
-        <div>
-          Directions: <input name="directions" type="text" />
+        <div className="mb-3">
+          Directions: <input className="form-control" name="directions" type="text" />
         </div>
-        <button type="submit">Create recipe</button>
+        <button className="btn btn-primary mb-3" type="submit">
+          Create recipe
+        </button>
       </form>
     </div>
   );
