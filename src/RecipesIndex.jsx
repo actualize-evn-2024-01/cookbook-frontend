e446401ad544d67bf7e33065c14dd3adcd379b16
Peter Jang <peter.x.jang@gmail.com>
2024-03-14 18:37:22 -0500

    Use bootstrap to style recipe index as cards in a grid

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -6,14 +6,19 @@ export function RecipesIndex(props) {
     <div id="recipes-index">
       <h1>All recipes</h1>
       <p>The name is {props.name}</p>
-      <div className="cards">
+
+      <div className="row row-cols-1 row-cols-md-4 g-4">
         {props.recipes.map((recipe) => (
-          <div key={recipe.id} className="recipes card">
-            <img src={recipe.image_url} alt="" />
-            <div className="card-body">
-              <h2>{recipe.title}</h2>
-              <p>Chef: {recipe.chef}</p>
-              <button onClick={() => props.onShowRecipe(recipe)}>More info</button>
+          <div key={recipe.id} className="col">
+            <div className="card">
+              <img src={recipe.image_url} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{recipe.title}</h5>
+                <p className="card-text">Chef: {recipe.chef}</p>
+                <button className="btn btn-primary" onClick={() => props.onShowRecipe(recipe)}>
+                  More info
+                </button>
+              </div>
             </div>
           </div>
         ))}
