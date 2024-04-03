74954900cac5ee161bb18ea6ca7e60939bf6f5df
Peter Jang <peter.x.jang@gmail.com>
2024-04-02 19:02:46 -0500

    Toggle authentication links based on jwt

--- src/Header.jsx
+++ src/Header.jsx
@@ -37,19 +37,24 @@ export function Header() {
                   New recipes
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup">
-                  Signup
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <LogoutLink />
-              </li>
+              {localStorage.getItem("jwt") ? (
+                <li className="nav-item">
+                  <LogoutLink />
+                </li>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/signup">
+                      Signup
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
