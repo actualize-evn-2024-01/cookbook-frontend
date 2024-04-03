708616e30548230dadbc9bc25a5f29d59bf0b4c6
Peter Jang <peter.x.jang@gmail.com>
2024-04-02 19:53:27 -0500

    Add example flash messages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [flashMessage, setFlashMessage] = useState("");
+
+  const handleFlashMessage = () => {
+    if (localStorage.getItem("flashMessage")) {
+      setFlashMessage(localStorage.getItem("flashMessage"));
+      localStorage.removeItem("flashMessage");
+    }
+  };
+
+  useEffect(handleFlashMessage, []);
+
   return (
     <div>
       <BrowserRouter>
         <Header />
+        {flashMessage ? (
+          <div onClick={() => setFlashMessage(null)} className="alert alert-primary" role="alert">
+            {flashMessage}
+          </div>
+        ) : null}
         <Content />
         <Footer />
       </BrowserRouter>
